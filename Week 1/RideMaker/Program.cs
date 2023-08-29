d18318848ec2572f6defd4ad949aba202fb3cdbf
OWEN LANG <owenlang66@gmail.com>
2023-08-28 18:44:40 -0700

    init

---
+++ Week 1/RideMaker/Program.cs
@@ -0,0 +1,20 @@
+﻿Vehicle Car1 = new Vehicle("Crap Car", 1, "Brown", false, 500000);
+Vehicle Car2 = new Vehicle("Sabrina", "pink");
+Vehicle Car3 = new Vehicle("Jim", "purple");
+Vehicle Car4 = new Vehicle("Mop", "green");
+
+List<Vehicle> vehicles = new List<Vehicle>();
+
+vehicles.Add(Car1);
+vehicles.Add(Car2);
+vehicles.Add(Car3);
+vehicles.Add(Car4);
+
+foreach (Vehicle vehicle in vehicles)
+{
+    vehicle.ShowInfo();
+}
+
+vehicles[2].Travel(100);
+
+vehicles[2].ShowInfo();
