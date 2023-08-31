f9a118e7b73f7d9a3ebd37c5b2b73a4c2997dbac
OWEN LANG <owenlang66@gmail.com>
2023-08-31 11:14:59 -0700

    init

--- Week 1/DateValidator/Controllers/HomeController.cs
+++ Week 1/DateValidator/Controllers/HomeController.cs
@@ -6,26 +6,23 @@ namespace DateValidator.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
+    // this httpget is just the landing page
+    [HttpGet("")]
+    public ViewResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpPost("submission")]
+    public IActionResult Submission(DateModel form)
     {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        if(ModelState.IsValid)
+        {
+            return View("Success", form);
+        }
+        else
+        {
+            return View("Index");
+        }
     }
 }
--- Week 1/DateValidator/Models/DateModel.cs
+++ Week 1/DateValidator/Models/DateModel.cs
@@ -1,8 +1,15 @@
 #pragma warning disable CS8618
 
+using System.ComponentModel.DataAnnotations;
+
 namespace DateValidator.Models;
 
 public class DateModel
 {
-    
+    [Required]
+    public string Name {get;set;}
+    [Required]
+    [DataType(DataType.DateTime)]
+    [Range(typeof(DateTime), "0001-12-31", "2023-08-31",  ErrorMessage = "Date must be in the past")]
+    public DateTime Date {get;set;}
 }
