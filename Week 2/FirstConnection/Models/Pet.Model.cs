109ba5ba95e5627d4e70e755dbbbd0841be6f81b
OWEN LANG <owenlang66@gmail.com>
2023-09-05 14:23:49 -0700

    First Connection Final

--- Week 2/FirstConnection/Models/Pet.Model.cs
+++ Week 2/FirstConnection/Models/Pet.Model.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 namespace FirstConnection.Models;
 public class Pet 
 {
+    [Key]
+    public int PetId {get;set;}
     public string Name {get;set;}
     public string Type {get;set;}
     public int Age {get;set;}
@@ -12,12 +14,3 @@ public class Pet
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
 
-public class MyContext : DbContext 
-{   
-    // This line will always be here. It is what constructs our context upon initialization  
-    public MyContext(DbContextOptions options) : base(options) { }    
-    // We need to create a new DbSet<Model> for every model in our project that is making a table
-    // The name of our table in our database will be based on the name we provide here
-    // This is where we provide a plural version of our model to fit table naming standards    
-    public DbSet<Pet> Pets { get; set; } 
-}
--- Week 2/FirstConnection/Program.cs
+++ Week 2/FirstConnection/Program.cs
@@ -1,4 +1,18 @@
+// Add this using statement
+using Microsoft.EntityFrameworkCore;
+// You will need access to your models for your context file
+using FirstConnection.Models;
+// Builder code from before
 var builder = WebApplication.CreateBuilder(args);
+// Create a variable to hold your connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// All your builder.services go here
+// And we will add one more service
+// Make sure this is BEFORE var app = builder.Build()!!
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
